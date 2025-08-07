9b7b4adfa1f88335f2696a77b0c3c825b188c3a3
tomasbennett <tjs.bennett@gmail.com>
2025-08-07 18:30:33 +0100

    Adding the iframe skeleton and styling to the main project frame

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./Pages/HTML/index.html";
 
 import "./Pages/CSS/style.css";
+import "./Pages/CSS/footer.css";
+import "./Pages/CSS/header.css";
+import "./Pages/CSS/projects.css";
 
 import "./TypeScript/Main.ts";
